42ae9f66fb16f4d5cb549712fce4d58ec7087f7e
natsu2597 <atulrishi1997@gmail.com>
2026-06-19 09:54:39 +0530

    initial commit

---
+++ CommandNexus.Platform/src/CommandNexus.Platform.Service/Data/AppDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CommandNexus.Platform.Service.Models;
+
+namespace CommandNexus.Platform.Service.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> opt) : base(opt)
+        {
+            
+        }
+
+        public DbSet<PlatformModel> Platforms { get; set; }
+    }
+}
